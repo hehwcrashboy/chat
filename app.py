b20de91a4ae9581a7b86085535ea68082a3386a0
hehwcrashboy <47809978+hehwcrashboy@users.noreply.github.com>
2023-03-19 19:44:16 +0800

    Update app.py

--- app.py
+++ app.py
@@ -7,6 +7,32 @@ import torch
 if "chat_history" not in st.session_state:
     st.session_state.chat_history = [{"role": "system", "content": "You are a helpful assistant."}]
 
+tokenizer = AutoTokenizer.from_pretrained("bert-base-cased-finetuned-mrpc")
+model = AutoModelForSequenceClassification.from_pretrained("bert-base-cased-finetuned-mrpc")
+
+def is_question_related(new_question, chat_history):
+    threshold = 0.85
+    # Retrieve the last user message from chat history
+    last_user_message = ""
+    for message in reversed(chat_history):
+        if message["role"] == "user":
+            last_user_message = message["content"]
+            break
+
+    # If there is no previous user message, return False
+    if not last_user_message:
+        return False
+
+    # Compute the similarity between the new question and the last user message
+    text_pair = [new_question, last_user_message]
+    encoded_input = tokenizer(text_pair, padding=True, truncation=True, return_tensors="pt")
+    with torch.no_grad():
+        scores = model(**encoded_input)[0]
+
+    similarity_score = torch.softmax(scores, dim=1).tolist()[0][1]
+
+    return similarity_score > threshold
+
 def chat_with_gpt3(messages):
     model_engine = "gpt-3.5-turbo"
 
@@ -38,6 +64,13 @@ if submit_button:
     max_history_length = 10
     if len(st.session_state.chat_history) > max_history_length:
         st.session_state.chat_history.pop(0)
+
+    # Check if the new question is related to the previous ones
+    related = is_question_related(user_input, st.session_state.chat_history)
+
+    # If the new question is not related, remove previous user messages from chat history
+    if not related:
+        st.session_state.chat_history = [message for message in st.session_state.chat_history if message["role"] != "user"]
 
     # Get model response and add it to chat history
     response = chat_with_gpt3(st.session_state.chat_history)
