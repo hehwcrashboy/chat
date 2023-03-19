881c284c736a5590211b043ec0daaec4ee386393
hehwcrashboy <47809978+hehwcrashboy@users.noreply.github.com>
2023-03-19 17:39:02 +0800

    Update app.py

--- app.py
+++ app.py
@@ -68,7 +68,7 @@ def display_conversation():
 
 display_conversation()
 
-user_input = input_container.text_input("Ask a question, enter a conversation, or request a translation:", value="", key="user_input")
+user_input = input_container.text_input("Ask a question, enter a conversation, or request a translation:", value="", key="bottom_user_input")
 submit_button = st.button("Submit", key="submit_button")
 
 input_container = st.empty()
